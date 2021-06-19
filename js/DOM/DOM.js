c9490ef5ba7e22e9bd41792dbb7ca2c1d3dbdc09
andrianholovchak <andrianholovchak@gmail.com>
2021-06-19 12:30:44 +0300

    push

---
+++ js/DOM/DOM.js
@@ -0,0 +1,55 @@
+/*
+a) отримує текст з параграфа з id "content"
+*/
+/*let allContent =  document.getElementById('content');
+console.log(allContent);*/
+
+/*
+b) отримує текст з блоку з id "rules"
+*/
+/*let allRules = document.getElementById('rules');
+console.log(allRules)*/
+/*
+c) замініть текст параграфа з id 'content' на будь-який інший
+*/
+
+/*let allContent = document.getElementById('content').innerText = 'Привіт мене звати Снуп мені 6 місяців'
+console.log(allContent);*/
+
+/*
+d) замініть текст параграфа з id 'rules' на будь-який інший
+*/
+
+/*let allRules = document.getElementById('rules').innerText = 'Ой чий то кінт стоїть . Що сива гривонька . Сподобалась мені , тая дівчинонька.'
+console.log(allRules);*/
+
+/*
+e) змініть кожному елементу колір фону на червоний
+*/
+/*
+document.body.style.backgroundColor = "red";
+*/
+/*
+f) змініть кожному елементу колір тексту на синій
+*/
+/*document.body.style.color = 'blue';*/
+/*
+g) отримати весь список класів елемента з id=rules і вивести їх в console.log
+*/
+/*let allRules = document.getElementById('rules');
+let rules = allRules.getElementsByClassName('fc bp');
+console.log(rules);*/
+/*
+h) отримати всі елементи з класом fc_rules
+*/
+
+/*let allFcRules = document.getElementsByClassName('fc_rules')
+console.log(allFcRules);*/
+
+/*
+i) поміняти колір тексту у всіх елементів fc_rules на червоний
+*/
+/*
+let allRules = document.getElementsByClassName('fc_rules')
+for (let i = 0; i < allRules.length; i++) {
+  allRules[i].style.backgroundColor = 'red';*!/*/
